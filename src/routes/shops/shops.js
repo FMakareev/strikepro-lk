2be01a288560e1ed855641c09abf06c7e9d678c8
FMakareev <f.makareev@compaero.ru>
2018-09-15 16:27:59 +0300

    Added closing the window, reset form, and updating the redux store after the successful creation of the new store. Added new properties for state management. Added store update

--- src/routes/shops/shops.js
+++ src/routes/shops/shops.js
@@ -1,219 +1,136 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {connect as connectRestEasy} from "@brigad/redux-rest-easy";
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect as connectRestEasy } from '@brigad/redux-rest-easy'
 
-import {StoreItem} from "../../blocks/store/store_item";
-import {PageTitle} from "../../blocks/PageTitle/PageTitle";
-import {StoreEdit} from "../../blocks/store/store_edit";
+import { StoreItem } from '../../blocks/store/store_item'
+import { PageTitle } from '../../blocks/PageTitle/PageTitle'
+import { StoreEdit } from '../../blocks/store/store_edit'
 import {
     GetStoreAction,
     CreateStoreAction,
     UpdateStoreAction,
     DeleteStoreAction,
     getStore,
-    isGetStore,
     isCreateStore,
     isUpdateStore,
     isDeleteStore,
-}from "../../store/reduxRestEasy/store";
-
-const store = [
-    {
-        id: "23121",
-        name: "Магазин 1",
-        address: "187050, Ленинградская обл, Тосненский р-н, Любань г, Ленина ул, дом № 17",
-        workinghours: [
-            {
-                type: 'monday',
-                from: '00:30',
-                to: '02:00'
-            },
-            {
-                type: 'tuesday',
-                from: '00:00',
-                to: '06:30'
-            },
-            {
-                type: 'wednesday',
-                from: '00:00',
-                to: '09:30'
-            },
-            {
-                type: 'thursday',
-                from: '00:30',
-                to: '04:00'
-            },
-            {
-                type: 'friday',
-                from: '00:30',
-                to: '06:30'
-            },
-            {
-                type: 'saturday',
-                from: '00:30',
-                to: '06:00'
-            },
-            {
-                type: 'sunday',
-                from: '00:00',
-                to: '06:30'
-            }
-        ],
-    }, {
-        id: "23121",
-        name: "Магазин 1",
-        address: "187050, Ленинградская обл, Тосненский р-н, Любань г, Ленина ул, дом № 17",
-        workinghours: [
-            {
-                type: 'monday',
-                from: '00:30',
-                to: '02:00'
-            },
-            {
-                type: 'tuesday',
-                from: '00:00',
-                to: '06:30'
-            },
-            {
-                type: 'wednesday',
-                from: '00:00',
-                to: '09:30'
-            },
-            {
-                type: 'thursday',
-                from: '00:30',
-                to: '04:00'
-            },
-            {
-                type: 'friday',
-                from: '00:30',
-                to: '06:30'
-            },
-            {
-                type: 'saturday',
-                from: '00:30',
-                to: '06:00'
-            },
-            {
-                type: 'sunday',
-                from: '00:00',
-                to: '06:30'
-            }
-        ],
-    }, {
-        id: "23121",
-        name: "Магазин 1",
-        address: "187050, Ленинградская обл, Тосненский р-н, Любань г, Ленина ул, дом № 17",
-        workinghours: [
-            {
-                type: 'monday',
-                from: '00:30',
-                to: '02:00'
-            },
-            {
-                type: 'tuesday',
-                from: '00:00',
-                to: '06:30'
-            },
-            {
-                type: 'wednesday',
-                from: '00:00',
-                to: '09:30'
-            },
-            {
-                type: 'thursday',
-                from: '00:30',
-                to: '04:00'
-            },
-            {
-                type: 'friday',
-                from: '00:30',
-                to: '06:30'
-            },
-            {
-                type: 'saturday',
-                from: '00:30',
-                to: '06:00'
-            },
-            {
-                type: 'sunday',
-                from: '00:00',
-                to: '06:30'
-            }
-        ],
-    }
-];
-
+    ResetStores,
+    getMetadataGetStore,
+    isPerformingGetStore,
+    hasSucceededGetStore,
+    isValidGetStore,
+    getResourceGetStore,
+    couldPerformGetStore, hasFailedGetStore
+} from '../../store/reduxRestEasy/store'
 
 @connectRestEasy(
-    (state, ownProps) => ({
-        store: getStore(state),
-        isGetStore:isGetStore(state, ownProps),
-        isCreateStore: isCreateStore(state, ownProps),
-        isUpdateStore: isUpdateStore(state, ownProps),
-        isDeleteStore: isDeleteStore(state, ownProps),
-    }),
-    dispatch => ({
-        CreateStoreAction: body => dispatch(CreateStoreAction({body})),
-        UpdateStoreAction: (body,urlParams) => dispatch(UpdateStoreAction({urlParams,body})),
-        DeleteStoreAction: (urlParams) => dispatch(DeleteStoreAction({urlParams})),
-        GetStoreAction: () => dispatch(GetStoreAction()),
-    })
+  (state, ownProps) => ({
+    store: getStore(state, ownProps),
+      getResourceGetStore: getResourceGetStore(state, ownProps),
+      getMetadataGetStore: getMetadataGetStore(state, ownProps),
+      couldPerformGetStore: couldPerformGetStore(state, ownProps),
+      isPerformingGetStore: isPerformingGetStore(state, ownProps),
+      hasSucceededGetStore: hasSucceededGetStore(state, ownProps),
+      hasFailedGetStore: hasFailedGetStore(state, ownProps),
+      isValidGetStore: isValidGetStore(state, ownProps),
+    isCreateStore: isCreateStore(state, ownProps),
+    isUpdateStore: isUpdateStore(state, ownProps),
+    isDeleteStore: isDeleteStore(state, ownProps)
+  }),
+  dispatch => ({
+    CreateStoreAction: body => dispatch(CreateStoreAction({ body })),
+    UpdateStoreAction: (body, urlParams) =>
+      dispatch(UpdateStoreAction({ urlParams, body })),
+    DeleteStoreAction: urlParams => dispatch(DeleteStoreAction({ urlParams })),
+    GetStoreAction: () => dispatch(GetStoreAction()),
+    ResetStore: () => dispatch(ResetStores())
+  })
 )
 class Shops extends Component {
+  static propTypes = {
+    GetStoreAction: PropTypes.func.isRequired,
+    CreateStoreAction: PropTypes.func.isRequired,
+    UpdateStoreAction: PropTypes.func.isRequired,
+    DeleteStoreAction: PropTypes.func.isRequired,
+    store: PropTypes.array,
+    isCreateStore: PropTypes.bool.isRequired,
+    isUpdateStore: PropTypes.bool.isRequired,
+    isDeleteStore: PropTypes.bool.isRequired
+  }
 
-    static propTypes = {
-        GetStoreAction: PropTypes.func.isRequired,
-        CreateStoreAction: PropTypes.func.isRequired,
-        UpdateStoreAction: PropTypes.func.isRequired,
-        DeleteStoreAction: PropTypes.func.isRequired,
-        store: PropTypes.array,
-        isGetStore: PropTypes.bool.isRequired,
-        isCreateStore: PropTypes.bool.isRequired,
-        isUpdateStore: PropTypes.bool.isRequired,
-        isDeleteStore: PropTypes.bool.isRequired,
-    }
+  constructor (props) {
+    super(props)
+    this.state = this.initialState
+    this.onDelete = this.onDelete.bind(this)
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = this.initialState;
-    }
+  get initialState () {
+    return {}
+  }
+  componentDidMount () {
+    this.props.GetStoreAction()
+  }
 
-    get initialState() {
-        return {}
-    }
-    componentDidMount(){
-        this.props.GetStoreAction();
-    }
+  onDelete (id) {
+    this.props
+      .DeleteStoreAction({ id })
+      .then(response => {
+        console.log('response', response)
+        this.props.ResetStore()
+        this.props
+          .GetStoreAction()
+          .then(res => {
+            console.log('res', res)
+          })
+          .catch(error => {
+            console.error('error', error)
+          })
+      })
+      .catch(error => {
+        console.error('error', error)
+      })
+  }
+  render () {
+    const { store } = this.props
+    console.log('store: ', store)
+    return (
+      <div id='body-container' className='animsition dashboard-page'>
 
-    render() {
-        console.log(this.props);
-        const {store} = this.props;
-        return (
-            <div id="body-container" className="animsition dashboard-page">
+        <PageTitle>
+          Магазины
+          <div
+            className='float-right'
+            style={{
+              margin: '-4px 0 0 0',
+              float: 'right'
+            }}
+          >
+            <StoreEdit className='modal-lg' buttonLabel={'Добавить магазин'} />
+          </div>
+        </PageTitle>
+        <div className="row">
+          <div className='panel panel-default'>
+          </div>
+          </div>
 
-                <PageTitle>
-                    Магазины
-                    <div className="float-right" style={{
-                        margin: "-4px 0 0 0",
-                        float: 'right'
-                    }}>
-                        <StoreEdit className="modal-lg" buttonLabel={"Добавить магазин"}/>
-                    </div>
-                </PageTitle>
+        <div className='row'>
+          {store &&
+            Object.values(store).map((item, index) => (
+              <StoreItem
+                onDelete={() => this.onDelete(item.id)}
+                key={index}
+                data={item}
+              />
+            ))}
+        </div>
 
-                <div className="row">
-                    {store && store.map((item, index) => <StoreItem key={index} data={item}/>)}
-                </div>
-
-
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
-Shops.propTypes = {};
+Shops.propTypes = {}
 
-Shops.defaultProps = {};
+Shops.defaultProps = {}
 
-export {Shops};
+export { Shops }
