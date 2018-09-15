34b42ee235293ff0d82c0fabb0f6a6ce84dc4b0f
FMakareev <f.makareev@compaero.ru>
2018-09-15 16:34:20 +0300

    Add initialValues

--- src/blocks/store/store_edit.js
+++ src/blocks/store/store_edit.js
@@ -1,47 +1,61 @@
-import React, {Component} from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import './store_edit.css';
-import {FormStoreEdit} from "../form/form_store-edit";
+import React, { Component } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
+import './store_edit.css'
+import { FormStoreEdit } from '../form/form_store-edit'
 class StoreEdit extends Component {
+  constructor (props) {
+    super(props)
+    this.state = this.initialState
+    this.toggle = this.toggle.bind(this)
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = this.initialState;
-        this.toggle = this.toggle.bind(this);
+  get initialState () {
+    return {
+      modal: false
+      // backdrop: 'static'
     }
+  }
 
-    get initialState() {
-        return {
-            modal: false,
-            // backdrop: 'static'
-        };
+  toggle () {
+    this.setState({
+      modal: !this.state.modal
+    })
+  }
+
+  render () {
+    const { data } = this.props
+    console.log(data)
+    let initialValues = {}
+    if (data) {
+      initialValues = {
+        address: data.address,
+        name: data.name,
+          workinghours: data.workinghours,
+        id: data.id
+      }
+      console.log('initialValues', initialValues)
     }
+    return (
+      <div style={{ display: 'inline-block' }}>
+        <Button color='warning' onClick={this.toggle}>
+          {this.props.buttonLabel}
+        </Button>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>О магазине</ModalHeader>
 
-    toggle() {
-        this.setState({
-            modal: !this.state.modal
-        });
-    }
-
-    render() {
-        console.log(this.props);
-        return (
-            <div style={{display: "inline-block"}}>
-                <Button color="warning" onClick={this.toggle}>{this.props.buttonLabel}</Button>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-                    <ModalHeader toggle={this.toggle}>О магазине</ModalHeader>
-
-                        <FormStoreEdit
-                            initialValues={this.props.data ? this.props.data: {}}
-                            toggleModal={this.toggle}/>
-                </Modal>
-            </div>
-        );
-    }
+                <FormStoreEdit initialValues={initialValues} toggleModal={this.toggle} />
+        </Modal>
+      </div>
+    )
+  }
 }
 
-StoreEdit.propTypes = {};
+StoreEdit.propTypes = {}
 
-StoreEdit.defaultProps = {};
+StoreEdit.defaultProps = {}
 
-export {StoreEdit};
+export { StoreEdit }
