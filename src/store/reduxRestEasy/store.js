7a7f2f82b72eeac2ede0f538ecf1a356fb96d200
FMakareev <f.makareev@compaero.ru>
2018-09-15 16:33:48 +0300

    Add normalizer date. Add request http headers 'Content-type'. Add new method management state

--- src/store/reduxRestEasy/store.js
+++ src/store/reduxRestEasy/store.js
@@ -1,99 +1,185 @@
-import {normalize} from "normalizr";
-import {createResource,} from '@brigad/redux-rest-easy';
-import {getToken} from "./networkHelpers/getToken";
-import login from "./login";
+import {createResource} from '@brigad/redux-rest-easy'
+import normalize from 'json-api-normalizer'
+import {getToken} from './networkHelpers/getToken'
 
-const store = createResource('store')({
+const store = createResource('store', {cacheLifetime: 0})({
     getStore: {
         method: 'GET',
-        url: 'http://new.strikepro.ru/api/v1/stores',
+        url: '/api/v1/stores',
         afterHook: () => console.log('Get stores successfully'),
+        normalizer: response => {
+            console.log('response', response)
+
+            let store = normalize(response).stores
+
+            if (!store) {
+                return {}
+            }
+            store = Object.values(store).map(item => ({
+                id: item.id,
+                address: item.attributes.address,
+                name: item.attributes.name,
+                workinghours: JSON.parse(item.attributes.workinghours),
+                createdAt: item.attributes.createdAt.date,
+                updatedAt: item.attributes.updatedAt.date
+            }))
+
+            let data = {
+                entities: {store: {}},
+                result: store.map(item => item.id)
+            }
+            store.forEach(element => {
+                data.entities.store[element.id] = element
+            })
+
+            return data
+        },
         networkHelpers: {
             getToken: getToken,
             async requestGET() {
-                let CurrentUser = await this.getToken().then(res => res);
+                let CurrentUser = await this.getToken().then(res => res)
+                console.log('CurrentUser', CurrentUser)
                 return {
                     headers: {
                         Accept: 'application/json',
-                        Authorization: `Bearer ${CurrentUser.access_token}`,
-                    },
-                };
-            },
-        },
+                        'Content-type': 'application/json',
+                        Authorization: `Bearer ${CurrentUser.access_token}`
+                    }
+                }
+            }
+        }
     },
     createStore: {
         method: 'POST',
-        url: 'http://new.strikepro.ru/api/v1/store/',
-        afterHook: () => console.log('Get stores successfully'),
+        url: '/api/v1/store/',
+        afterHook: () => console.log('POST stores successfully'),
+        normalizer: response => {
+            console.log('PUT response', response)
+            let store = response
+            store.workinghours = JSON.parse(store.workinghours)
+
+            let data = {
+                entities: {
+                    store: {
+                        [store.id]: store
+                    }
+                },
+                result: [store.id]
+            }
+            console.log('data', data)
+
+            return data
+        },
         networkHelpers: {
             getToken: getToken,
             async requestPOST(body) {
-                let CurrentUser = await this.getToken().then(res => res);
-                console.log('createStore: ',body);
+                let CurrentUser = await this.getToken().then(res => res)
+                console.log('createStore: ', body)
                 return {
                     method: 'POST',
                     headers: {
                         Accept: 'application/json',
-                        Authorization: `Bearer ${CurrentUser.access_token}`,
+                        'Content-type': 'application/json',
+                        Authorization: `Bearer ${CurrentUser.access_token}`
                     },
-                    body: JSON.stringify(body),
-                };
-            },
-        },
+                    body: JSON.stringify(body)
+                }
+            }
+        }
     },
     updateStore: {
         method: 'PUT',
-        url: 'http://new.strikepro.ru/api/v1/store/::id',
-        afterHook: () => console.log('Get stores successfully'),
+        url: '/api/v1/store/::id',
+        afterHook: () => console.log('PUT stores successfully'),
+        normalizer: response => {
+            console.log('PUT response', response)
+            let store = response
+            store.workinghours = JSON.parse(store.workinghours)
+
+            let data = {
+                entities: {
+                    store: {
+                        [store.id]: store
+                    }
+                },
+                result: [store.id]
+            }
+            console.log('data', data)
+
+            return data
+        },
         networkHelpers: {
             getToken: getToken,
             async requestPUT(body) {
-                let CurrentUser = await this.getToken().then(res => res);
+                let CurrentUser = await this.getToken().then(res => res)
                 return {
                     method: 'PUT',
                     headers: {
                         Accept: 'application/json',
-                        Authorization: `Bearer ${CurrentUser.access_token}`,
+                        'Content-type': 'application/json',
+                        Authorization: `Bearer ${CurrentUser.access_token}`
                     },
-                    body: JSON.stringify(body),
-                };
-            },
-        },
+                    body: JSON.stringify(body)
+                }
+            }
+        }
     },
     deleteStore: {
         method: 'DELETE',
-        url: 'http://new.strikepro.ru/api/v1/store/::id',
-        afterHook: () => console.log('Get stores successfully'),
+        url: '/api/v1/store/::id',
+        afterHook: (
+            normalizedPayload,
+            urlParams,
+            query,
+            body,
+            otherArgs,
+            dispatch
+        ) => {
+            console.log('DELETE stores normalizedPayload', normalizedPayload)
+            console.log('DELETE stores urlParams', urlParams)
+            console.log('DELETE stores query', query)
+            console.log('DELETE stores body', body)
+            console.log('DELETE stores otherArgs', otherArgs)
+            console.log('DELETE stores dispatch', dispatch)
+
+            console.log('DELETE stores successfully', arguments)
+        },
         networkHelpers: {
             getToken: getToken,
             async requestDELETE() {
-                let CurrentUser = await this.getToken().then(res => res);
+                let CurrentUser = await this.getToken().then(res => res)
                 return {
                     method: 'DELETE',
                     headers: {
                         Accept: 'application/json',
-                        Authorization: `Bearer ${CurrentUser.access_token}`,
-                    },
-                };
-            },
-        },
-    },
-});
+                        'Content-type': 'application/json',
+                        Authorization: `Bearer ${CurrentUser.access_token}`
+                    }
+                }
+            }
+        }
+    }
+})
 
 const {
     actions: {
+        resource: {
+            invalidate: invalidateUsers,
+            invalidateId: invalidateUser,
+            reset: ResetStores
+        },
         getStore: {
-            perform: GetStoreAction,
+            perform: GetStoreAction
         },
         createStore: {
-            perform: CreateStoreAction,
+            perform: CreateStoreAction
         },
         updateStore: {
-            perform: UpdateStoreAction,
+            perform: UpdateStoreAction
         },
         deleteStore: {
-            perform: DeleteStoreAction,
-        },
+            perform: DeleteStoreAction
+        }
     },
     selectors: {
         resource: {
@@ -101,26 +187,32 @@ const {
         },
         getStore: {
             request: {
-                isPerforming: isGetStore,
-            },
+                getResource:    getResourceGetStore,
+                getMetadata:    getMetadataGetStore,
+                couldPerform:   couldPerformGetStore,
+                isPerforming:   isPerformingGetStore,
+                hasSucceeded:   hasSucceededGetStore,
+                hasFailed:      hasFailedGetStore,
+                isValid:        isValidGetStore,
+            }
         },
         createStore: {
             request: {
-                isPerforming: isCreateStore,
-            },
+                isPerforming: isCreateStore
+            }
         },
         updateStore: {
             request: {
-                isPerforming: isUpdateStore,
-            },
+                isPerforming: isUpdateStore
+            }
         },
         deleteStore: {
             request: {
-                isPerforming: isDeleteStore,
-            },
-        },
-    },
-} = store;
+                isPerforming: isDeleteStore
+            }
+        }
+    }
+} = store
 
 export {
     GetStoreAction,
@@ -128,8 +220,15 @@ export {
     UpdateStoreAction,
     DeleteStoreAction,
     getStore,
-    isGetStore,
+    getResourceGetStore,
+    getMetadataGetStore,
+    couldPerformGetStore,
+    isPerformingGetStore,
+    hasSucceededGetStore,
+    hasFailedGetStore,
+    isValidGetStore,
     isCreateStore,
     isUpdateStore,
     isDeleteStore,
+    ResetStores
 }
