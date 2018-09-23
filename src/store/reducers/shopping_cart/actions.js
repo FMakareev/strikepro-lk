be130d3f21241c937afb2ea7532d13d7b51bbb61
FMakareev <f.makareev@compaero.ru>
2018-09-23 20:20:42 +0300

    Replaced the implementation of the initialization of the recycle bin. Renamed variables: sum => total_price, allCount => total_products. Added the method of updating the order on the server. I have overloaded unnecessary imports and logs

--- src/store/reducers/shopping_cart/actions.js
+++ src/store/reducers/shopping_cart/actions.js
@@ -1,129 +1,136 @@
 import {
-    INIT_SHOPPING_CART_LOAD,
-    INIT_SHOPPING_CART_SUCCESS,
-    INIT_SHOPPING_CART_ERROR,
+  INIT_SHOPPING_CART_LOAD,
+  INIT_SHOPPING_CART_SUCCESS,
+  INIT_SHOPPING_CART_ERROR,
 
-    SHOPPING_CART_ADD_LOAD,
-    SHOPPING_CART_ADD_SUCCESS,
-    SHOPPING_CART_ADD_ERROR,
+  SHOPPING_CART_ADD_LOAD,
+  SHOPPING_CART_ADD_SUCCESS,
+  SHOPPING_CART_ADD_ERROR,
 
-    SHOPPING_CART_REMOVE_LOAD,
-    SHOPPING_CART_REMOVE_SUCCESS,
-    SHOPPING_CART_REMOVE_ERROR,
 
 } from './action_types';
 import {UPDATE_PRODUCT} from "../catalog_products/action_type";
+import {UpdateOrderAction} from "../../reduxRestEasy/order";
 
 
-export const initShoppingCart = (state) => {
-    return (dispatch) => {
+export const initShoppingCart = (data) => {
+  return (dispatch) => {
+    dispatch({
+      type: INIT_SHOPPING_CART_LOAD,
+      payload: true
+    });
+    return new Promise((resolve, reject) => {
+      try {
+        if (data) {
+          dispatch({
+            type: INIT_SHOPPING_CART_SUCCESS,
+            payload: {
+              order: data
+            }
+          });
+        } else {
+          dispatch({
+            type: INIT_SHOPPING_CART_SUCCESS,
+            payload: {
+              order: null
+            }
+          });
+        }
+        resolve(true)
+      } catch (e) {
         dispatch({
-            type: INIT_SHOPPING_CART_LOAD,
-            payload: true
+          type: INIT_SHOPPING_CART_ERROR,
+          payload: {
+            order: null,
+            error: e,
+          }
         });
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                dispatch({
-                    type: INIT_SHOPPING_CART_SUCCESS,
-                    payload: {
-                        order: {
-                            id: 123,
-                            store_id: 1,
-                            count: 0,
-                            sum: 0,
-                            products: []
-                        }
-                    }
-                });
-                resolve(true)
-            }, 5000)
-        })
-    }
+        reject(e);
+      }
+    })
+  }
 };
 
 
 export const updateProduct = (state, product, count) => {
-    return (dispatch) => {
+  return (dispatch) => {
 
-        try {
-            // if (!state.shopping_cart.load) {
-            dispatch({
-                type: SHOPPING_CART_ADD_LOAD,
-                payload: true
-            });
-            const State = JSON.parse(JSON.stringify(state));
-            const {
-                shopping_cart: {order},
-                catalog_products,
-            } = State;
+    try {
+      // if (!state.shopping_cart.load) {
+      dispatch({
+        type: SHOPPING_CART_ADD_LOAD,
+        payload: true
+      });
+      /** @desc клонирую стэйт */
+      const State = JSON.parse(JSON.stringify(state));
+      let {
+        shopping_cart: {order},
+        catalog_products, /** @description Продукты отображаемые в данный момент в каталоге*/
+      } = State;
 
-            // Если нахожу продукт в корзине обновляю запись
-            if (order.products.find((obj) => obj.id === product.id)) {
-                order.products.forEach((obj) => {
-                    if (obj.id === product.id) {
-                        obj.count = count;
-                    }
-                });
-            } else {
-                product.count = count;
-                order.products.push(product)
-            }
-            catalog_products.products.forEach((obj) => {
-                if (obj.id === product.id) {
-                    obj.count = count;
-                }
-            });
-            let sum = 0;
-            let allCount = 0;
+      console.log('order prev: ', order);
+      /** @desc Если нахожу продукт в корзине обновляю запись*/
+      if (order.products.find((obj) => obj.id === product.id)) {
+        order.products.forEach((obj) => {
+          if (obj.id === product.id) {
+            obj.count = count;
+          }
+        });
+      } else {
+        product.count = count;
+        order.products.push(product)
+      }
 
-            console.log(order);
-            order.products.forEach((obj) => {
-                console.log(obj);
-                sum = (parseFloat(sum) + (parseFloat(obj.count) * parseFloat(obj.price.price))).toFixed(2);
-                allCount =  parseInt(allCount) + parseInt(obj.count);
-            });
-
-            order.count = allCount;
-            order.sum = sum;
-            console.log('order: ', order);
-            console.log(catalog_products);
-
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    dispatch({
-                        type: SHOPPING_CART_ADD_SUCCESS,
-                        payload: {
-                            order
-                        }
-                    });
-                    dispatch({
-                        type: UPDATE_PRODUCT,
-                        payload: catalog_products
-                    });
-                    resolve(true)
-                }, 10)
-            })
-            // }
-
-        } catch (error) {
-            dispatch({
-                type: SHOPPING_CART_ADD_ERROR,
-                payload: error
-            });
+      catalog_products.products.forEach((obj) => {
+        if (obj.id === product.id) {
+          obj.count = count;
         }
+      });
+      let total_price = 0;
+      let total_products = 0;
+
+      /** @desc удоляем продукты колличество которых равно 0 */
+      order.products = order.products.filter((item) => item.count !== 0);
+
+      order.products.forEach((obj) => {
+        total_price = (parseFloat(total_price) + (parseInt(obj.count) * parseFloat(obj.price.price).toFixed(2))).toFixed(2);
+        total_products = parseInt(total_products) + parseInt(obj.count);
+      });
+
+      order.total_products = total_products;
+      order.total_price = total_price;
+
+      return new Promise((resolve, reject) => {
+        dispatch(UpdateOrderAction({body: order}))
+          .then((response) => {
+            console.log(response);
+            dispatch({
+              type: SHOPPING_CART_ADD_SUCCESS,
+              payload: {
+                order
+              }
+            });
+            dispatch({
+              type: UPDATE_PRODUCT,
+              payload: catalog_products
+            });
+            resolve(true)
+          }).catch(error => {
+          console.log(error);
+          reject(error);
+          dispatch({
+            type: SHOPPING_CART_ADD_ERROR,
+            payload: error
+          });
+        });
+      })
+
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: SHOPPING_CART_ADD_ERROR,
+        payload: error
+      });
     }
+  }
 };
-
-const ShoppingCart = (order) => {
-    return (dispatch) => {
-
-    }
-}
-
-
-const removeProduct = (state, product, count) => {
-    return (dispatch) => {
-
-    }
-}
-
