f8ed56e6d9b3938c3c6bc3aa2208647713f9aab4
FMakareev <f.makareev@compaero.ru>
2018-10-14 22:25:28 +0300

    Add try catch in updateProduct

--- src/store/reducers/shopping_cart/actions.js
+++ src/store/reducers/shopping_cart/actions.js
@@ -1,11 +1,11 @@
 import {
-  INIT_SHOPPING_CART_LOAD,
-  INIT_SHOPPING_CART_SUCCESS,
-  INIT_SHOPPING_CART_ERROR,
+	INIT_SHOPPING_CART_LOAD,
+	INIT_SHOPPING_CART_SUCCESS,
+	INIT_SHOPPING_CART_ERROR,
 
-  SHOPPING_CART_ADD_LOAD,
-  SHOPPING_CART_ADD_SUCCESS,
-  SHOPPING_CART_ADD_ERROR,
+	SHOPPING_CART_ADD_LOAD,
+	SHOPPING_CART_ADD_SUCCESS,
+	SHOPPING_CART_ADD_ERROR,
 
 
 } from './action_types';
@@ -14,123 +14,132 @@ import {UpdateOrderAction} from "../../reduxRestEasy/order";
 
 
 export const initShoppingCart = (data) => {
-  return (dispatch) => {
-    dispatch({
-      type: INIT_SHOPPING_CART_LOAD,
-      payload: true
-    });
-    return new Promise((resolve, reject) => {
-      try {
-        if (data) {
-          dispatch({
-            type: INIT_SHOPPING_CART_SUCCESS,
-            payload: {
-              order: data
-            }
-          });
-        } else {
-          dispatch({
-            type: INIT_SHOPPING_CART_SUCCESS,
-            payload: {
-              order: null
-            }
-          });
-        }
-        resolve(true)
-      } catch (e) {
-        dispatch({
-          type: INIT_SHOPPING_CART_ERROR,
-          payload: {
-            order: null,
-            error: e,
-          }
-        });
-        reject(e);
-      }
-    })
-  }
+	return (dispatch) => {
+		dispatch({
+			type: INIT_SHOPPING_CART_LOAD,
+			payload: true
+		});
+		return new Promise((resolve, reject) => {
+			try {
+				if (data) {
+					dispatch({
+						type: INIT_SHOPPING_CART_SUCCESS,
+						payload: {
+							order: data
+						}
+					});
+				} else {
+					dispatch({
+						type: INIT_SHOPPING_CART_SUCCESS,
+						payload: {
+							order: null
+						}
+					});
+				}
+				resolve(true)
+			} catch (e) {
+				dispatch({
+					type: INIT_SHOPPING_CART_ERROR,
+					payload: {
+						order: null,
+						error: e,
+					}
+				});
+				reject(e);
+			}
+		})
+	}
 };
 
 
 export const updateProduct = (state, product, count) => {
-  return (dispatch) => {
+	return (dispatch) => {
 
-    try {
-      // if (!state.shopping_cart.load) {
-      dispatch({
-        type: SHOPPING_CART_ADD_LOAD,
-        payload: true
-      });
-      /** @desc клонирую стэйт */
-      const State = JSON.parse(JSON.stringify(state));
-      let {
-        shopping_cart: {order},
-        catalog_products, /** @description Продукты отображаемые в данный момент в каталоге*/
-      } = State;
+		try {
+			// if (!state.shopping_cart.load) {
+			dispatch({
+				type: SHOPPING_CART_ADD_LOAD,
+				payload: true
+			});
+			/** @desc клонирую стэйт */
+			const State = JSON.parse(JSON.stringify(state));
+			let {
+				shopping_cart: {order},
+				catalog_products, /** @description Продукты отображаемые в данный момент в каталоге*/
+			} = State;
 
-      console.log('order prev: ', order);
-      /** @desc Если нахожу продукт в корзине обновляю запись*/
-      if (order.products.find((obj) => obj.id === product.id)) {
-        order.products.forEach((obj) => {
-          if (obj.id === product.id) {
-            obj.count = count;
-          }
-        });
-      } else {
-        product.count = count;
-        order.products.push(product)
-      }
+			console.log('order prev: ', order);
+			/** @desc Если нахожу продукт в корзине обновляю запись*/
+			if (order.products.find((obj) => obj.id === product.id)) {
+				order.products.forEach((obj) => {
+					if (obj.id === product.id) {
+						obj.count = count;
+					}
+				});
+			} else {
+				product.count = count;
+				order.products.push(product)
+			}
 
-      catalog_products.products.forEach((obj) => {
-        if (obj.id === product.id) {
-          obj.count = count;
-        }
-      });
-      let total_price = 0;
-      let total_products = 0;
+			catalog_products.products.forEach((obj) => {
+				if (obj.id === product.id) {
+					obj.count = count;
+				}
+			});
+			let total_price = 0;
+			let total_products = 0;
 
-      /** @desc удоляем продукты колличество которых равно 0 */
-      order.products = order.products.filter((item) => item.count !== 0);
+			/** @desc удоляем продукты колличество которых равно 0 */
+			order.products = order.products.filter((item) => item.count !== 0);
 
-      order.products.forEach((obj) => {
-        total_price = (parseFloat(total_price) + (parseInt(obj.count) * parseFloat(obj.price.price).toFixed(2))).toFixed(2);
-        total_products = parseInt(total_products) + parseInt(obj.count);
-      });
+			order.products.forEach((obj) => {
+				total_price = (parseFloat(total_price) + (parseInt(obj.count) * parseFloat(obj.price.price).toFixed(2))).toFixed(2);
+				total_products = parseInt(total_products) + parseInt(obj.count);
+			});
 
-      order.total_products = total_products;
-      order.total_price = total_price;
+			order.total_products = total_products;
+			order.total_price = total_price;
 
-      return new Promise((resolve, reject) => {
-        dispatch(UpdateOrderAction({body: order}))
-          .then((response) => {
-            console.log(response);
-            dispatch({
-              type: SHOPPING_CART_ADD_SUCCESS,
-              payload: {
-                order
-              }
-            });
-            dispatch({
-              type: UPDATE_PRODUCT,
-              payload: catalog_products
-            });
-            resolve(true)
-          }).catch(error => {
-          console.log(error);
-          reject(error);
-          dispatch({
-            type: SHOPPING_CART_ADD_ERROR,
-            payload: error
-          });
-        });
-      })
+			try {
+				return new Promise((resolve, reject) => {
+					dispatch(UpdateOrderAction({body: order}))
+						.then((response) => {
+							console.log(response);
+							dispatch({
+								type: SHOPPING_CART_ADD_SUCCESS,
+								payload: {
+									order
+								}
+							});
+							dispatch({
+								type: UPDATE_PRODUCT,
+								payload: catalog_products
+							});
+							resolve(true)
+						}).catch(error => {
+						console.log(error);
+						reject(error);
+						dispatch({
+							type: SHOPPING_CART_ADD_ERROR,
+							payload: error
+						});
+					});
+				})
 
-    } catch (error) {
-      console.log(error);
-      dispatch({
-        type: SHOPPING_CART_ADD_ERROR,
-        payload: error
-      });
-    }
-  }
+			} catch (error) {
+				console.log(error);
+				dispatch({
+					type: SHOPPING_CART_ADD_ERROR,
+					payload: error
+				});
+			}
+
+		} catch (error) {
+			console.log(error);
+			dispatch({
+				type: SHOPPING_CART_ADD_ERROR,
+				payload: error
+			});
+		}
+	}
 };
