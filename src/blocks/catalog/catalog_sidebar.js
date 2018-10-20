c0c868f5ea93cd1a8999e90564509751ca6d9456
FMakareev <f.makareev@compaero.ru>
2018-10-20 19:42:45 +0300

    Format

--- src/blocks/catalog/catalog_sidebar.js
+++ src/blocks/catalog/catalog_sidebar.js
@@ -1,101 +1,136 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {Treebeard} from 'react-treebeard';
+import { Treebeard } from 'react-treebeard';
 import style from './catalog_tree-menu-style';
-import {connect} from "react-redux";
-import {getCategory, initCategory, onToggle} from "../../store/reducers/catalog_category/actions";
-import {Store} from "../../store/store";
+import { connect } from "react-redux";
+import { getCategory, initCategory, onToggle } from "../../store/reducers/catalog_category/actions";
+import { Store } from "../../store/store";
+
 const data = {
-    name: 'root',
-    toggled: true,
-    children: [
-        {
-            name: 'parent',
-            children: [
-                {name: 'child1'},
-                {name: 'child2'}
-            ]
-        },
-        {
-            name: 'loading parent',
-            loading: true,
-            children: []
-        },
-        {
-            name: 'parent',
-            children: [
-                {
-                    name: 'nested parent',
-                    children: [
-                        {name: 'nested child 1'},
-                        {name: 'nested child 2'}
-                    ]
-                }
-            ]
-        }
-    ]
+	name: 'root',
+	toggled: true,
+	children: [
+		{
+			name: 'parent',
+			children: [
+				{name: 'child1'},
+				{name: 'child2'}
+			]
+		},
+		{
+			name: 'loading parent',
+			loading: true,
+			children: []
+		},
+		{
+			name: 'parent',
+			children: [
+				{
+					name: 'nested parent',
+					children: [
+						{name: 'nested child 1'},
+						{name: 'nested child 2'}
+					]
+				}
+			]
+		}
+	]
+};
+const decorators = {
+	// Loading: (props) => {
+	// 	return (
+	// 		<div style={{
+	// 			...props.style,
+	// 			backgroundColor: 'purple'
+	// 		}}>
+	// 			loading...
+	// 		</div>
+	// 	);
+	// },
+	// Toggle: (props) => {
+	// 	return (
+	// 		<div style={{
+	// 			...props.style,
+	// 			backgroundColor: 'red'
+	// 		}}>
+	// 			<svg height={props.height} width={props.width}>
+	// 				// Vector Toggle Here
+	// 			</svg>
+	// 		</div>
+	// 	);
+	// },
+	Header: (props) => {
+		console.log(props);
+		return (
+			<div style={{
+				...props.style,
+				backgroundColor: 'green'
+			}}>
+				{props.node && props.node.name}
+			</div>
+		);
+	},
 };
 
-
 @connect(
-    state => ({ // получаем данные из store
-        tree: state.catalog_category.tree
-    }), //
-    dispatch => ({
-        getCategory: (id) => {
-            dispatch(getCategory(Store.getState(), id))
-        },
-        onToggle: (node, toggled) => {
-            dispatch(onToggle(node, toggled))
-        },
-        initCategory: () => {
-            dispatch(initCategory(Store.getState()))
-        },
-        setStore: (type, value) => {
-            dispatch({type: type, payload: value})
-        }
-    })
+	state => ({ // получаем данные из store
+		tree: state.catalog_category.tree
+	}), //
+	dispatch => ({
+		getCategory: (id) => {
+			dispatch(getCategory(Store.getState(), id))
+		},
+		onToggle: (node, toggled) => {
+			dispatch(onToggle(node, toggled))
+		},
+		initCategory: () => {
+			dispatch(initCategory(Store.getState()))
+		},
+		setStore: (type, value) => {
+			dispatch({type: type, payload: value})
+		}
+	})
 )
 export class CatalogSidebar extends Component {
 
-    static propTypes = {
-        onToggle: PropTypes.func.isRequired,
-        initCategory: PropTypes.func.isRequired,
-        tree: PropTypes.object.isRequired,
-    };
-    static defaultProps = {};
+	static propTypes = {
+		onToggle: PropTypes.func.isRequired,
+		initCategory: PropTypes.func.isRequired,
+		tree: PropTypes.object.isRequired,
+	};
+	static defaultProps = {};
 
-    constructor(props) {
-        super(props);
-        this.state = this.initialState;
-    }
+	constructor(props) {
+		super(props);
+		this.state = this.initialState;
+	}
 
-    get initialState() {
-        return {
+	get initialState() {
+		return {}
+	}
 
-        }
-    }
+	componentDidMount() {
+		try {
+			if (!this.props.tree.children.length) {
+				this.props.initCategory();
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-    componentDidMount() {
-        try{
-            if(!this.props.tree.children.length){
-                this.props.initCategory();
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    }
+	render() {
+		const {tree, onToggle} = this.props;
+		console.log('CatalogSidebar this.props.tree', this.props.tree);
+		return (
+			<div className="catalog_sidebar panel panel-default">
+				<Treebeard
 
-    render() {
-        const {tree,onToggle} = this.props;
-        return (
-            <div className="catalog_sidebar panel panel-default">
-                <Treebeard
-                    data={tree}
-                    onToggle={onToggle}
-                    style={style}
-                />
-            </div>
-        )
-    }
+					data={tree}
+					onToggle={onToggle}
+					style={style}
+				/>
+			</div>
+		)
+	}
 }
